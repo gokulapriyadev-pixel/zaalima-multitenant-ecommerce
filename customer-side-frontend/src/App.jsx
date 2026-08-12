4885915159780fcbff72db8b7eb238804cc2bd40
codesuraj12 <160992864+codesuraj12@users.noreply.github.com>
2026-08-12 13:38:16 +0530

    Updated layout

--- customer-side-frontend/src/App.jsx
+++ customer-side-frontend/src/App.jsx
@@ -1,28 +1,33 @@
 import { useState } from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Regiser'
 import Footer from './components/Footer'
+import StoreLayout from './components/StoreLayout'
 
 function App() {
 
 
   return (
     <>
- <BrowserRouter>
-      <Navbar />
+      <BrowserRouter>
 
-      <Routes>
-        <Route path= "/" element={<Home />} />
-        <Route path= "/login" element={<Login />} />
-        <Route path= "/register" element={<Register />} />
-      </Routes>
-<Footer />
+   <Routes>
 
-    </BrowserRouter>
+          <Route element={<StoreLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
